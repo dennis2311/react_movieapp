8b0b68a677d59207cc92c7de2bd3c015ff8501cb
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2020-07-22 16:37:25 +0900

    state change

--- src/App.js
+++ src/App.js
@@ -1,49 +1,27 @@
 import React from 'react';
 import propTypes from "prop-types";
 
-function Food(props){
-  return <div>
-    <h3>I love {props.name}</h3>
-    <h4>{props.rating}/5.0</h4>
-    <img src={props.picture} alt={props.name}/>
-  </div>;
-}
+class App extends React.Component{
+  state = {  //changable data
+    count:0,
+  }
 
-const foodList = [
-  {
-    id:1,
-    name:"kimchi",
-    picture: "",
-    rating: 5
-  },
-  {
-    id:2,
-    name:"garlic",
-    picture:"",
-    rating:2.2
-  },
-  {
-    id:3,
-    name:"pork",
-    picture:"https://cafedelites.com/wp-content/uploads/2019/12/Pork-Loin-Roast-Recipe-1.jpg",
-    rating: 8
-  }];
+  //setState refreshes page with new state and calls render function again
+  add = () => {  //using 'current' is better way
+    this.setState(current => ({count: current.count+1}));
+  }
+  minus = () => {
+    this.setState({count: this.state.count-1});
+  }
 
-Food.propTypes = {
-  name: propTypes.string.isRequired,
-  picture: propTypes.string.isRequired,
-  rating: propTypes.string.isRequired
-}
-
-
-function App() {
-  return(
-  <div>
-    {foodList.map(food =>
-      <Food key={food.id} name={food.name} picture={food.picture} rating={food.rating}/>
-    )}
-  </div>
-  );
+  render(){  //react automatically execute render method
+    return <div>
+        <h1>I'm a class componet</h1>
+        <h4>I have number {this.state.count}</h4>
+        <button onClick={this.add}>ADD</button>
+        <button onClick={this.minus}>MINUS</button>
+      </div>;
+  }
 }
 
 export default App;
